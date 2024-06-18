dc372c6cde9782df71240ba3bd86f0722163cce9
uidk4271 <alexander.2.ransmann@continental-corporation.com>
2024-06-18 08:48:11 +0200

    Update names

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,19 +19,19 @@ kicad-cli pcb export gerbers --no-protel-ext --output "$HOME/tempGerbers/" "$pro
 # Drill files
 kicad-cli pcb export drill --output "$HOME/tempGerbers/" "$projectName.kicad_pcb"
 # Pack it
-zip -r "$HOME/$outputDirectory/$today-$version-$projectName-Gerbers-Drills.zip" "$HOME/tempGerbers/"
+zip -r "$HOME/$outputDirectory/$today-$projectName-$version-Gerbers-Drills.zip" "$HOME/tempGerbers/"
 
 # Position files
-kicad-cli pcb export pos --exclude-dnp --output "$HOME/$outputDirectory/$today-$version-$projectName-Placement.pos" "$projectName.kicad_pcb"
+kicad-cli pcb export pos --exclude-dnp --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement.pos" "$projectName.kicad_pcb"
 # Placement documents
-kicad-cli pcb export pdf --layers "F.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$version-$projectName-Placement-Top.pdf" "$projectName.kicad_pcb"
-kicad-cli pcb export pdf --layers "B.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$version-$projectName-Placement-Bottom.pdf" "$projectName.kicad_pcb"
+kicad-cli pcb export pdf --layers "F.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement-Top.pdf" "$projectName.kicad_pcb"
+kicad-cli pcb export pdf --layers "B.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement-Bottom.pdf" "$projectName.kicad_pcb"
 
 # 3D files
-kicad-cli pcb export vrml --output "$HOME/$outputDirectory/$today-$version-$projectName-3D-Model.vrml" "$projectName.kicad_pcb"
+kicad-cli pcb export vrml --output "$HOME/$outputDirectory/$today-$projectName-$version-3D-Model.vrml" "$projectName.kicad_pcb"
 
 # Schematic
-kicad-cli sch export pdf --output "$HOME/$outputDirectory/$today-$version-$projectName-Schematic.pdf" "$projectName.kicad_sch"
+kicad-cli sch export pdf --output "$HOME/$outputDirectory/$today-$projectName-$version-Schematic.pdf" "$projectName.kicad_sch"
 
 # BOM
-kicad-cli sch export bom --fields $BomFormat --exclude-dnp --output "$HOME/$outputDirectory/$today-$version-$projectName-BOM.csv" "$projectName.kicad_sch"
+kicad-cli sch export bom --fields $BomFormat --exclude-dnp --output "$HOME/$outputDirectory/$today-$projectName-$version-BOM.csv" "$projectName.kicad_sch"
