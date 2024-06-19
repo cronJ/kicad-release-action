9e2fba8eafcf3e931b376e4a55d38b7dc4d78ee2
uidk4271 <alexander.2.ransmann@continental-corporation.com>
2024-06-19 07:17:05 +0200

    Change file location

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 FROM kicad/kicad:8.0
 
-USER root
-
 RUN apt-get update \
     && apt-get install -y zip \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-RUN mkdir -p /output
-
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
-
-VOLUME ["/output"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,31 +11,31 @@ echo "Version: $version"
 echo "Output Directory: $outputDirectory"
 echo "Today: $today"
 
-mkdir -p $HOME/$outputDirectory
-mkdir -p $HOME/tempGerbers
+mkdir -p /github/workspace/$outputDirectory
+mkdir -p /github/workspace/tempGerbers
 
 ls -al
 
 # Gerber files
-kicad-cli pcb export gerbers --no-protel-ext --output "$HOME/tempGerbers/" "$projectName.kicad_pcb"
+kicad-cli pcb export gerbers --no-protel-ext --output "/github/workspace/tempGerbers/" "$projectName.kicad_pcb"
 # Drill files
-kicad-cli pcb export drill --output "$HOME/tempGerbers/" "$projectName.kicad_pcb"
+kicad-cli pcb export drill --output "/github/workspace/tempGerbers/" "$projectName.kicad_pcb"
 # Pack it
-zip -r "$HOME/$outputDirectory/$today-$projectName-$version-Gerbers-Drills.zip" "$HOME/tempGerbers/"
+zip -r "/github/workspace/$outputDirectory/$today-$projectName-$version-Gerbers-Drills.zip" "/github/workspace/tempGerbers/"
 
 # Position files
-kicad-cli pcb export pos --exclude-dnp --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement.pos" "$projectName.kicad_pcb"
+kicad-cli pcb export pos --exclude-dnp --output "/github/workspace/$outputDirectory/$today-$projectName-$version-Placement.pos" "$projectName.kicad_pcb"
 # Placement documents
-kicad-cli pcb export pdf --layers "F.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement-Top.pdf" "$projectName.kicad_pcb"
-kicad-cli pcb export pdf --layers "B.Silkscreen,Edge.Cuts" --black-and-white --output "$HOME/$outputDirectory/$today-$projectName-$version-Placement-Bottom.pdf" "$projectName.kicad_pcb"
+kicad-cli pcb export pdf --layers "F.Silkscreen,Edge.Cuts" --black-and-white --output "/github/workspace/$outputDirectory/$today-$projectName-$version-Placement-Top.pdf" "$projectName.kicad_pcb"
+kicad-cli pcb export pdf --layers "B.Silkscreen,Edge.Cuts" --black-and-white --output "/github/workspace/$outputDirectory/$today-$projectName-$version-Placement-Bottom.pdf" "$projectName.kicad_pcb"
 
 # 3D files
-kicad-cli pcb export vrml --output "$HOME/$outputDirectory/$today-$projectName-$version-3D-Model.vrml" "$projectName.kicad_pcb"
+kicad-cli pcb export vrml --output "/github/workspace/$outputDirectory/$today-$projectName-$version-3D-Model.vrml" "$projectName.kicad_pcb"
 
 # Schematic
-kicad-cli sch export pdf --output "$HOME/$outputDirectory/$today-$projectName-$version-Schematic.pdf" "$projectName.kicad_sch"
+kicad-cli sch export pdf --output "/github/workspace/$outputDirectory/$today-$projectName-$version-Schematic.pdf" "$projectName.kicad_sch"
 
 # BOM
-kicad-cli sch export bom --fields $BomFormat --exclude-dnp --output "$HOME/$outputDirectory/$today-$projectName-$version-BOM.csv" "$projectName.kicad_sch"
+kicad-cli sch export bom --fields $BomFormat --exclude-dnp --output "/github/workspace/$outputDirectory/$today-$projectName-$version-BOM.csv" "$projectName.kicad_sch"
 
-ls -al $HOME/$outputDirectory
+ls -al /github/workspace/$outputDirectory
